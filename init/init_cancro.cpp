e299718ddbe9188d48e26258ecfd8bfd4a8ee1b1
DrRamm <kotsareu@gmail.com>
2017-01-13 20:10:17 +0300

    cancro: init: set right name for MI 4CDMA
    Change-Id: I0db9518eb740899cf48a51ff2d6359316db085c1

--- init/init_cancro.cpp
+++ init/init_cancro.cpp
@@ -132,8 +132,11 @@ void vendor_load_properties()
         case 1974:
             property_override("ro.product.model", "MI 4");
             break;
-        case 1972:
-            property_override("ro.product.model", "MI 4LTE");
+        case 1973:
+            property_set("ro.product.model", "MI 4CDMA");
+            break;
+	case 1972:
+            property_set("ro.product.model", "MI 4LTE");
             property_set("ro.telephony.default_network", "8");
             property_set("telephony.lteOnGSMDevice", "1");
             break;
